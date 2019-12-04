6f4e0c856dfa8df763b978a84416e47d1c7a1c39
denstedttan <tanner1234545@gmail.com>
2019-12-04 10:44:53 -0600

    Add project files.

---
+++ Movies/Controllers/MoviesController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web.Mvc;
+using Movies.Models;
+using Movies.ViewModels;
+namespace Movies.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+
+            return View(movies);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Interstellar" },
+                new Movie { Id = 2, Name = "Space Jam" }
+            };
+        }
+
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Interstellar" };
+            var customers = new List<Customer>
+            {
+                new Customer { Name = "Customer 1" },
+                new Customer { Name = "Customer 2" }
+            };
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+            return View(viewModel);
+        }
+
+        public ActionResult Details(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
